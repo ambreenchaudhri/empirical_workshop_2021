318695be97b3d39a8d0de05de9728d1558e5c9ad
Ambreen Chaudhri <awc6034@klc03.quest.it.northwestern.edu>
2021-01-12 10:52:30 -0600

    Every 10 secs

--- 1_reproducibility_KLC_intro/time.py
+++ 1_reproducibility_KLC_intro/time.py
@@ -6,9 +6,9 @@
 import time
 
 # first statement
-print("This file tells you the time after every 20 seconds.")
+print("This file tells you the time after every 10 seconds.")
 
-# print the time after every 20 seconds
+# print the time after every 10 seconds
 for i in range(10000):
     print("The time is now: " + time.strftime("%X") + ". Time flies when you are on KLC.")
-    time.sleep(20)
+    time.sleep(10)
